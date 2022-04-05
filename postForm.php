102f963077dc75e1f2a4b3cff0ee19d1d4fa3bb1
JatiWahyuKusuma <jatiikusuma@gmail.com>
2022-04-05 08:34:31 +0700

    Jobshett7

---
+++ postForm.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+    </head>
+    <body>
+        Welcome <?php echo $_POST["Username"]; ?> !! <br>
+        Your E-mail addres is : <?php echo $_POST["email"]; ?>
+    </body>
+</html>
